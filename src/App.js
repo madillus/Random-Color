1c07ed3a1f095e8565dfbca4fc424440189ebcab
madillus <68782132+madillus@users.noreply.github.com>
2022-07-01 15:43:57 +0200

    Updated to react 18

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
 import './App.css';
+import randomColor from 'randomcolor';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+export default function App() {
+  const hue = [
+    'random',
+    'red',
+    'blue',
+    'green',
+    'orange',
+    'monochrome',
+    'pink',
+    'yellow',
+    'purple',
+  ]
+  const luminosity = ['random', 'light', 'dark', 'bright']
+  const [color, setColor] = useState(randomColor('random', 'random'))
+  const [hueColor, setHueColor] = useState(hue[0])
+  const [lum, setLum] = useState(luminosity[0])
+
+  useEffect(() => {
+    setColor(randomColor({ hue: hueColor, luminosity: lum }))
+  }, [hueColor, lum])
+
+  const handleHueChange = (e) => {
+    const newHue = e.target.value
+    setHueColor(newHue)
+  }
+
+  const handleLumChange = (e) => {
+    const newLum = e.target.value
+    setLum(newLum)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div
+      className="App"
+      style={{
+        minWidth: '100vw',
+        minHeight: '100vh',
+        backgroundColor: `${color}`,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+      }}
+    >
+      <h1> Random Color Generator </h1>
+      <h2> {color} </h2>
 
-export default App;
+      <div className="select-container">
+        <form>
+          <select
+            name="hue"
+            value={hueColor.value}
+            onChange={(e) => handleHueChange(e)}
+          >
+            {hue.map((hue, idx) => (
+              <option key={idx}>{hue}</option>
+            ))}
+          </select>
+        </form>
+        <form>
+          <select
+            name="luminosity"
+            value={lum.value}
+            onChange={(e) => handleLumChange(e)}
+          >
+            {luminosity.map((lum, idx) => (
+              <option key={idx}>{lum}</option>
+            ))}
+          </select>
+        </form>
+      </div>
+      <div className="button-container">
+        <button
+          className="glow-on-hover "
+          onClick={() =>
+            setColor(randomColor({ hue: hueColor, luminosity: lum }))
+          }
+        >
+          randomize
+        </button>
+        <button
+          className="glow-on-hover "
+          onClick={() => navigator.clipboard.writeText(color)}
+        >
+          Copy Color
+        </button>
+      </div>
+    </div>
+  )
+}
